91201c0f5aeb4e4bab58ae0f4a9e2faea7a7c2ba
ELFGGGAME <yotsakorn2641@gmail.com>
2026-08-09 15:40:22 +0700

    deployV.1

---
+++ nuxt-app/app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Index pages </h1>
+    <p>
+        <span>Name: </span>
+        <span>{{ name }}</span>
+    </p>
+    <bottom @click="handleClick('worked')"> rename </bottom>
+</template>
+    <bottom @click="handleShowLogName">console.log </bottom>
+
+<script setup>
+const name = ref("init")
+
+const handleClick = () => {
+    console.log('worked form handleClick function')
+
+    name.nalue = "yotsakorn"
+}
+</script>
---
+++ nuxt-app/app/pages/product.vue
@@ -0,0 +1,3 @@
+<template> 
+76oduct 
+</template>
